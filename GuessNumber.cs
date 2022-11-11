2b286e97a85f7e94d1f1673b9e69c6e9b0a536bb
IndreVilke <karcioji@gmail.com>
2022-11-11 15:02:12 +0200

    initial

---
+++ GuessNumber.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace GuessNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+
+	        Random random = new Random();
+
+            int winNum = random.Next(1, 20);
+            int guess = 0;
+            int numGuesses = 0;
+
+            Console.WriteLine("I am thinking of a number between 1-20.  Can you guess what it is?");
+
+            while (guess != winNum)
+            {
+                guess = Convert.ToInt32(Console.ReadLine());
+                
+
+                
+                numGuesses++;
+                    if (guess < winNum)
+                    {
+                        Console.WriteLine("No, the number I am thinking of is higher than " + guess + " .  Can you guess what it is?");
+                    }
+                    if (guess > winNum)
+                    {
+                        Console.WriteLine("No, the number I am thinking of is lower than " + guess + " .  Can you guess what it is");
+                    }
+             
+            }
+            Console.WriteLine("Well done! The answer was " + winNum + ".\nYou took " + numGuesses + " guesses.");
+        }
+
+
+    }
+}
