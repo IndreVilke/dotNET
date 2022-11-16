9548de9ed491d3e4313e9d6b9d9703c7ea78b76e
IndreVilke <karcioji@gmail.com>
2022-11-16 16:12:07 +0200

    initial calculator

---
+++ Calculator/calculator.cs
@@ -0,0 +1,24 @@
+﻿
+class Calculator
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Enter number");
+        string number1str = Console.ReadLine();
+        Console.WriteLine("Enter the second number");
+        string number2str = Console.ReadLine();
+        int number1 = Int32.Parse(number1str);
+        int number2 = Int32.Parse(number2str);
+        int sumresult = number1 + number2;
+        Console.WriteLine(number1 + "+" + number2 + "=" + result);
+        int difresult = number1 - number2;
+        Console.WriteLine(number1 + "-" + number2 + "=" + difresult);
+        int multresult = number1 * number2;
+        Console.WriteLine(number1 + "*" + number2 + "=" + multresult);
+        if number2 != 0:
+            int divresult = number1 / number2;
+            Console.WriteLine(number1 + "%" + number2 + "=" + divresult);
+        else:
+                skip;
+
---
+++ Calculator/calculator1/calculator1/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+class Calculator
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Enter number");
+        string number1str = Console.ReadLine();
+        Console.WriteLine("Enter the second number");
+        string number2str = Console.ReadLine();
+        int number1 = Int32.Parse(number1str);
+        int number2 = Int32.Parse(number2str);
+        int sumresult = number1 + number2;
+        Console.WriteLine(number1 + "+" + number2 + "=" + sumresult);
+        int difresult = number1 - number2;
+        Console.WriteLine(number1 + "-" + number2 + "=" + difresult);
+        int multresult = number1 * number2;
+        Console.WriteLine(number1 + "*" + number2 + "=" + multresult);
+        double divresult = ((double)number1 / number2);
+        Console.WriteLine(number1 + "/" + number2 + "=" + divresult);
+    }
+
+}
+
+
