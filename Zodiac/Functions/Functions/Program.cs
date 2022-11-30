59d26865ce1317ad1290633037731ae115e5ed44
IndreVilke <karcioji@gmail.com>
2022-11-30 20:23:38 +0200

    Factorial initial

---
+++ Zodiac/Functions/Functions/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string numberStr;
+            double number;
+
+            do
+            {
+                Console.WriteLine("enter the number");
+                numberStr = Console.ReadLine();
+            }
+
+            while (!double.TryParse(numberStr, out number));
+
+            Console.WriteLine(Factorial(number));
+        }
+
+        public static double Factorial(double n) 
+        {
+            if (n == 1)
+                return 1;
+            else
+                return n * Program.Factorial(n - 1);
+        
+        }
+
+
+
+    }
+}
