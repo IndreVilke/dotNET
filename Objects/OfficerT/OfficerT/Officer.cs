6312a8ec7bb6d8679a4fb878fea24fb593e7b5b6
IndreVilke <karcioji@gmail.com>
2023-01-01 22:21:33 +0200

    Officer at last

---
+++ Objects/OfficerT/OfficerT/Officer.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfficerT
+{
+    internal class Officer
+    {
+            public string name { get; }
+            public string surname { get; }
+            public int officerId { get; }
+            public string workingDistrict { get; }
+            public int crimesSolved { get; }
+
+            public Officer(string objName, string objSurname, int objOfficerId,
+                           string objWorkingDistrict, int objCrimesSolved)
+            {
+                this.name = objName;
+                this.surname = objSurname;
+                this.officerId = objOfficerId;
+                this.workingDistrict = objWorkingDistrict;
+                this.crimesSolved = objCrimesSolved;
+            }
+            public int CalculatedLevel()
+            {
+                if (crimesSolved < 20)
+                {
+                    return 1;
+
+                }
+                else if (crimesSolved < 40)
+                {
+                    return 2;
+                }
+                else
+                {
+                    return 3;
+                }
+
+
+            }
+            public override string ToString()
+            {
+                return $"Name: {name,-8} Surname: {surname,-10} OfficerID: {officerId} " +
+                   $"Working district: {workingDistrict}  Crimes solved: {crimesSolved}";
+            }
+            public static int Level1Count(List<Officer> distr)
+            {
+                int count = 0;
+                foreach (var officer in distr)
+                    if (officer.CalculatedLevel() == 1)
+                        count++;
+                return count;
+            }
+            public static int Level23Count(List<Officer> distr)
+            {
+                int count = 0;
+                foreach (var officer in distr)
+                    if (officer.CalculatedLevel() > 1)
+                        count++;
+                return count;
+            }
+            public static bool ExistName(List<Officer> distr, string name)
+            {
+                foreach (var officer in distr)
+                    if (officer.name == name)
+                        return true;
+                return false;
+            }
+        }
+
+
+
+    }
+
---
+++ Objects/OfficerT/OfficerT/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OfficerT
+    {
+       
+
+        class Program
+        {
+            public static void Main(string[] args)
+            {
+                List<Officer> district99 = new List<Officer>();
+                district99.Add(new Officer("Peters", "Ulmanis", 7068, "99", 7));
+                district99.Add(new Officer("John", "Turmanis", 7069, "99", 22));
+                district99.Add(new Officer("Algirdas", "Simonis", 7098, "99", 13));
+                district99.Add(new Officer("Peters", "Varmanis", 7088, "99", 15));
+
+                foreach (var officer in district99)
+                    Console.WriteLine(officer);
+
+                int k = Officer.Level1Count(district99);
+                Console.WriteLine("There is " + k + " Level 1 Officers");
+                k = Officer.Level23Count(district99);
+                Console.WriteLine("There is " + k + " higher then Level 1 Officers");
+
+                bool x = Officer.ExistName(district99, "John");
+                if (x == true)
+                    Console.WriteLine("There is Officer called John");
+                else
+                    Console.WriteLine("There is no Officer called John");
+
+            }
+        }
+    }
